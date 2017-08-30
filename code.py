6c3e58acbfcc0186c7ddbc8f1e690c519c6273e8
Mark <m.j.kuiack@uva.nl>
2017-08-30 11:28:10 -0700

    Update code.py
    fix indentation

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 
 def do_something():
     something = "do_something did something"# complete
-	print(“Hello World”)
+    print(“Hello World”)
     print(something)# this will make it much easier in future problems to see that something is actually happening
