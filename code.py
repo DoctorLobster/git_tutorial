cfcf99d97fd430ba1821042db2e8466c1a956290
Mark Kuiack <kuiack@Marks-MacBook-Pro-3.local>
2017-08-30 10:24:14 -0700

    Add code.py which does something

---
+++ code.py
@@ -0,0 +1,4 @@
+
+def do_something():
+    something = "do_something did something"# complete
+    print(something)# this will make it much easier in future problems to see that something is actually happening
